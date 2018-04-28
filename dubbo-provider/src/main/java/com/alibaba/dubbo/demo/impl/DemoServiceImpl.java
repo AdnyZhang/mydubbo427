928409a0b22bfe8171e9199c56e6a8f1622634aa
zhch-git <zhangchao@zhuojianchina.com>
2018-04-28 08:56:18 +0800

    create

--- dubbo-provider/src/main/java/com/alibaba/dubbo/demo/impl/DemoServiceImpl.java
+++ dubbo-provider/src/main/java/com/alibaba/dubbo/demo/impl/DemoServiceImpl.java
@@ -1,9 +1,6 @@
 package com.alibaba.dubbo.demo.impl;
-
 import com.alibaba.dubbo.demo.DemoService;
-
 import java.util.ArrayList;
-
 import java.util.List;
 public class DemoServiceImpl implements DemoService {
     public List<String> getPermissions(Long id) {
